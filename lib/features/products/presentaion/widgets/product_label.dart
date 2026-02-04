227296b9d36d936384d63108542865d6288bc179
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2026-02-04 19:34:27 +0200

    create Product Label ui

---
+++ lib/features/products/presentaion/widgets/product_label.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/resources/color_manager.dart';
+import '../../../../core/resources/styles_manager.dart';
+
+class ProductLabel extends StatelessWidget {
+  const ProductLabel({
+    required this.name,
+    required this.price,
+  });
+
+  final String name;
+  final String price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: Text(
+            name,
+            style: getMediumStyle(color: ColorManager.primary)
+                .copyWith(fontSize: 18.sp),
+          ),
+        ),
+        Text(
+          price,
+          style: getMediumStyle(color: ColorManager.primary)
+              .copyWith(fontSize: 18.sp),
+        ),
+      ],
+    );
+  }
+}
