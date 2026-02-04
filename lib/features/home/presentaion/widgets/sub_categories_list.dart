5a6eface4c1f138e91b7cc305f07b3f2c898e59f
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2026-02-04 17:36:15 +0200

    create ui SubCategoriesList

---
+++ lib/features/home/presentaion/widgets/sub_categories_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/resources/assets_manager.dart';
+import '../../../../core/resources/color_manager.dart';
+import '../../../../core/resources/font_manager.dart';
+import '../../../../core/resources/styles_manager.dart';
+import '../../../../core/resources/values_manager.dart';
+import 'category_card.dart';
+
+class SubCategoriesList extends StatelessWidget {
+  const SubCategoriesList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Text(
+              'Laptops',
+              style: getBoldStyle(
+                color: ColorManager.primary,
+                fontSize: FontSize.s14,
+              ),
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: CategoryCardItem(
+              'Laptops',
+              ImageAssets.categoryCardImage,
+            ),
+          ),
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 26,
+                  (_, index) => const SubCategoryItem(
+                'Watches',
+                ImageAssets.subcategoryCardImage,
+              ),
+            ),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 0.75,
+              mainAxisSpacing: Sizes.s8.h,
+              crossAxisSpacing: Sizes.s8.w,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
