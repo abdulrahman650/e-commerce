444968be46200f4886c5419da760f54483cdf908
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2026-02-04 17:48:06 +0200

    create wish item details ui

---
+++ lib/features/wishlist/presentaion/widgets/wishlist_item_details.dart
@@ -0,0 +1,90 @@
+import 'package:colornames/colornames.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/resources/color_manager.dart';
+import '../../../../core/resources/font_manager.dart';
+import '../../../../core/resources/styles_manager.dart';
+import '../../../../core/resources/values_manager.dart';
+import 'custom_auto_size_text.dart';
+
+class WishlistItemDetails extends StatelessWidget {
+  const WishlistItemDetails({required this.product});
+
+  final Map<String, dynamic> product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        CustomAutoSizeText(
+          data: product['title'],
+          textStyle: getSemiBoldStyle(
+            color: ColorManager.primaryDark,
+            fontSize: FontSize.s18,
+          ),
+        ),
+        Row(
+          children: [
+            Container(
+              margin: EdgeInsetsDirectional.only(end: Sizes.s10.w),
+              width: Sizes.s14.w,
+              height: Sizes.s14.h,
+              decoration: BoxDecoration(
+                color: product['color'],
+                shape: BoxShape.circle,
+              ),
+            ),
+            CustomAutoSizeText(
+              data: (product['color'] as Color).colorName,
+              textStyle: getMediumStyle(
+                color: ColorManager.primaryDark,
+                fontSize: FontSize.s14,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            CustomAutoSizeText(
+              data: 'EGP ${product['finalPrice']}  ',
+              textStyle: getSemiBoldStyle(
+                color: ColorManager.primaryDark,
+                fontSize: FontSize.s18,
+              ).copyWith(
+                letterSpacing: 0.17,
+              ),
+            ),
+            product['salePrice'] == null
+                ? const SizedBox.shrink()
+                : Flexible(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: Sizes.s10.h,
+                  ),
+                  CustomAutoSizeText(
+                    data: 'EGP ${product['salePrice']}',
+                    textStyle: getMediumStyle(
+                      color: ColorManager.appBarTitle.withOpacity(.6),
+                    ).copyWith(
+                      letterSpacing: 0.17,
+                      decoration: TextDecoration.lineThrough,
+                      color: ColorManager.appBarTitle.withOpacity(.6),
+                      fontSize: FontSize.s10,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
