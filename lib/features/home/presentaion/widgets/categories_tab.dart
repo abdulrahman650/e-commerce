34682d2a6284419937dbfe14449dae8fbeab7a3a
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2026-02-04 17:29:31 +0200

    create ui categories tab

---
+++ lib/features/home/presentaion/widgets/categories_tab.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/resources/values_manager.dart';
+
+class CategoriesTab extends StatelessWidget {
+  const CategoriesTab();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: Insets.s12.w,
+        vertical: Insets.s12.h,
+      ),
+      child: Row(
+        children: [
+          const CategoriesList(),
+          SizedBox(
+            width: Sizes.s16.w,
+          ),
+          const SubCategoriesList(),
+        ],
+      ),
+    );
+  }
+}
